487ef63db66aa3c6df88021a88ff47928a349ad1
Akbalaev Atabek <akbalaevatabek@gmail.com>
2021-11-19 15:26:24 +0600

    Added hero and dragon gameplay

--- homework_19/src/com/company/Main.java
+++ homework_19/src/com/company/Main.java
@@ -10,30 +10,59 @@ public class Main {
         int[][] damages = new int[2][100];
         Scanner scanner = new Scanner(System.in);
         Hero hero = new Hero(1000, 100, 120, 250, 150);
-        Dragon dragon = new Dragon(2000, 120, 150, 100);
-        int damageToDragon = 0;
-        int damageToHero = 0;
-        System.out.println("\nGame started... Initial state of the game:");
-        printInfo(move, hero, damageToDragon, dragon, damageToHero);
+        Dragon dragon = new Dragon(2000, 120, 150, 120);
+
+        System.out.println("Game started... Initial state of the game:");
+        printInfo(move, hero, 0, dragon, 0);
+
         while(true) {
-            if (dragon.getHealthPoints() == 0) {
-                System.out.println("\nThe game has ended.");
+            if (didHeroWin(dragon)) {
+                System.out.println("Hero has won. The end.");
                 break;
             }
-            if (new Random().nextInt(4) > 0) { // 75%, 1,2,3 except 0
-                damageToDragon = hero.getStrength() + hero.getWeapon() - dragon.getDefense();
-                System.out.println("\nThe Hero has hit the dragon...");
-            } else {
-                damageToDragon = 0;
-                System.out.println("\nThe Hero has missed...");
+            if (didDragonWin(hero)) {
+                System.out.println("Dragon has won. The end.");
+                break;
             }
-            int hpDragonTemp = dragon.getHealthPoints() - damageToDragon;
-            int dragonHP = hpDragonTemp < 0 ? 0 : hpDragonTemp;
-            dragon.setHealthPoints(dragonHP);
             move++;
-            printInfo(move, hero, damageToDragon, dragon, damageToHero);
+            int amountOfAttackFromHero = heroToDragonAttackAmount(hero, dragon);
+            int amountOfAttackFromDragon = dragonToHeroAttackAmount(hero, dragon);
+            printInfo(move, hero, amountOfAttackFromHero, dragon, amountOfAttackFromDragon);
         }
-
+    }
+    static int dragonToHeroAttackAmount(Hero hero, Dragon dragon) {
+        int damage;
+        if (new Random().nextInt(2) == 0) {
+            damage = dragon.getStrength() + dragon.getWeapon() - hero.getDefense();
+            System.out.println("The Dragon has attacked the Hero...");
+        } else {
+            damage = 0;
+            System.out.println("The Dragon didn't attack...");
+        }
+        int hpLeft = hero.getHealthPoints() - damage;
+        int heroHP = Math.max(0, hpLeft);
+        hero.setHealthPoints(heroHP);
+        return damage;
+    }
+    static int heroToDragonAttackAmount(Hero hero, Dragon dragon) {
+        int damage;
+        if (new Random().nextInt(4) > 0) { // 75%, 1,2,3 except 0
+            damage = hero.getStrength() + hero.getWeapon() - dragon.getDefense();
+            System.out.println("\nThe Hero has attacked the Dragon...");
+        } else {
+            damage = 0;
+            System.out.println("\nThe Hero has missed...");
+        }
+        int hpLeft = dragon.getHealthPoints() - damage;
+        int dragonHP = Math.max(0, hpLeft);
+        dragon.setHealthPoints(dragonHP);
+        return damage;
+    }
+    static boolean didDragonWin(Hero hero) {
+        return hero.getHealthPoints() == 0;
+    }
+    static boolean didHeroWin(Dragon dragon) {
+        return dragon.getHealthPoints() == 0;
     }
     static void printInfo(int move, Hero hero, int damageToDragon, Dragon dragon, int damageToHero) {
         String l1 = "+----+" + "-".repeat(25) + "+" + "-".repeat(16) + "++" +
