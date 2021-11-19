5d97be0b38ae8430316996277b1960ecdecea579
Akbalaev Atabek <akbalaevatabek@gmail.com>
2021-11-19 18:40:25 +0600

    Added Hellhound

---
+++ homework_19/src/com/company/Hellhound.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Hellhound extends PlayerType{
+    public Hellhound(int healthPoints, int strength, int defense, int weapon) {
+        super(healthPoints, strength, defense, weapon);
+    }
+}
--- homework_19/src/com/company/Main.java
+++ homework_19/src/com/company/Main.java
@@ -7,15 +7,26 @@ public class Main {
 
     public static void main(String[] args) {
         int move = 0;
-        int[][] damages = new int[2][100];
-        Scanner scanner = new Scanner(System.in);
+        String enemy;
         Hero hero = new Hero(1000, 100, 120, 250, 100);
-        Dragon dragon = new Dragon(2000, 150, 150, 120);
+        Dragon dragon = new Dragon(1400, 150, 150, 120);
+        Hellhound hellhound = new Hellhound(1200, 120, 100, 150);
 
         System.out.println("Game started... Initial state of the game:");
-        printInfo(move, hero, 0, dragon, 0);
+        if (new Random().nextInt(2) == 0) {
+            enemy = "dragon";
+            System.out.println("Dragon was chosen by the computer.");
+            printInfo(move, hero, 0, dragon, 0);
+        } else {
+            enemy = "hellhound";
+            System.out.println("Hellhound was chosen by the computer.");
+            printInfo(move, hero, 0, hellhound, 0);
+        }
 
         while(true) {
+            if (didHellhoundWin(hero)) {
+                System.out.println("Hellhound has won. The end.");
+            }
             if (didHeroWin(dragon)) {
                 System.out.println("Hero has won. The end.");
                 break;
@@ -26,9 +37,15 @@ public class Main {
             }
             move++;
             char userAction = getUserAction();
-            int amountOfAttackFromHero = heroToDragonAttackAmount(hero, dragon, userAction);
-            int amountOfAttackFromDragon = dragonToHeroAttackAmount(hero, dragon, userAction);
-            printInfo(move, hero, amountOfAttackFromHero, dragon, amountOfAttackFromDragon);
+            if (enemy.equals("dragon")) {
+                int amountOfAttackFromHero = heroToDragonAttackAmount(hero, dragon, userAction);
+                int amountOfAttackFromDragon = dragonToHeroAttackAmount(hero, dragon, userAction);
+                printInfo(move, hero, amountOfAttackFromHero, dragon, amountOfAttackFromDragon);
+            } else {
+                int amountOfAttackFromHero = heroToHellhoundAttackAmount(hero, hellhound, userAction);
+                int amountOfAttackFromHellhound = hellhoundToHeroAttackAmount(hero, hellhound, userAction);
+                printInfo(move, hero, amountOfAttackFromHero, hellhound, amountOfAttackFromHellhound);
+            }
         }
     }
     static char getUserAction() {
@@ -41,6 +58,21 @@ public class Main {
             option = scanner.nextLine().charAt(0);
         }
         return option;
+    }
+    static int hellhoundToHeroAttackAmount(Hero hero, Hellhound hellhound, char action) {
+        int damage = 0;
+        int shield = action == 'd' ? hero.getShield() : 0;
+        if (new Random().nextInt(2) == 0) {
+            damage = hellhound.getStrength() + hellhound.getWeapon() - (hero.getDefense() + shield);
+            System.out.println("The Hellhound has attacked the Hero...");
+        } else {
+            System.out.println("The Hellhound didn't attack...");
+        }
+        damage = Math.max(0, damage);
+        int hpLeft = hero.getHealthPoints() - damage;
+        int heroHP = Math.max(0, hpLeft);
+        hero.setHealthPoints(heroHP);
+        return damage;
     }
     static int dragonToHeroAttackAmount(Hero hero, Dragon dragon, char action) {
         int damage = 0;
@@ -55,6 +87,29 @@ public class Main {
         int hpLeft = hero.getHealthPoints() - damage;
         int heroHP = Math.max(0, hpLeft);
         hero.setHealthPoints(heroHP);
+        return damage;
+    }
+    static int heroToHellhoundAttackAmount(Hero hero, Hellhound hellhound, char action) {
+        int damage = 0;
+        switch (action) {
+            case 'a':
+                if (new Random().nextInt(4) > 0) { // 75%, 1,2,3 except 0
+                    damage = hero.getStrength() + hero.getWeapon() - hellhound.getDefense();
+                    System.out.println("\nThe Hero has attacked the Hellhound...");
+                } else {
+                    System.out.println("\nThe Hero has missed...");
+                }
+                break;
+            case 'n':
+                System.out.println("\nThe Hero has chosen to do nothing...");
+                break;
+            case 'd':
+                System.out.println("\nThe Hero is holding the shield...");
+        }
+        damage = Math.max(0, damage);
+        int hpLeft = hellhound.getHealthPoints() - damage;
+        int dragonHP = Math.max(0, hpLeft);
+        hellhound.setHealthPoints(dragonHP);
         return damage;
     }
     static int heroToDragonAttackAmount(Hero hero, Dragon dragon, char action) {
@@ -80,16 +135,31 @@ public class Main {
         dragon.setHealthPoints(dragonHP);
         return damage;
     }
+    static boolean didHellhoundWin(Hero hero) {
+        return hero.getHealthPoints() == 0;
+    }
     static boolean didDragonWin(Hero hero) {
         return hero.getHealthPoints() == 0;
     }
     static boolean didHeroWin(Dragon dragon) {
         return dragon.getHealthPoints() == 0;
     }
+    static void printInfo(int move, Hero hero, int damageToHellhound, Hellhound hellhound, int damageToHero) {
+        String l1 = "+----+" + "-".repeat(28) + "+" + "-".repeat(16) + "++" +
+                "-".repeat(28) + "+" + "-".repeat(16) + "+";
+        String header = "|%4s|%28s|%15s ||%28s|%15s |\n";
+        System.out.println(l1);
+        System.out.printf(header, "Move", "Hero's damage to Hellhound", "Hero's HP",
+                "Hellhound's damage to Hero", "Hellhound's HP");
+        System.out.println(l1);
+        System.out.printf(header, move, damageToHellhound, hero.getHealthPoints(),
+                damageToHero, hellhound.getHealthPoints());
+        System.out.println(l1);
+    }
     static void printInfo(int move, Hero hero, int damageToDragon, Dragon dragon, int damageToHero) {
-        String l1 = "+----+" + "-".repeat(25) + "+" + "-".repeat(16) + "++" +
-                "-".repeat(25) + "+" + "-".repeat(16) + "+";
-        String header = "|%4s|%25s|%15s ||%25s|%15s |\n";
+        String l1 = "+----+" + "-".repeat(28) + "+" + "-".repeat(16) + "++" +
+                "-".repeat(28) + "+" + "-".repeat(16) + "+";
+        String header = "|%4s|%28s|%15s ||%28s|%15s |\n";
         System.out.println(l1);
         System.out.printf(header, "Move", "Hero's damage to Dragon", "Hero's HP",
                 "Dragon's damage to Hero", "Dragon's HP");
