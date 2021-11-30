05efdf6435b90f0bae7070c09105c98b194a4f2a
Akbalaev Atabek <akbalaevatabek@gmail.com>
2021-11-30 17:42:43 +0600

    fixed change hand

--- Homework22/src/homework22/level4/Game.java
+++ Homework22/src/homework22/level4/Game.java
@@ -29,17 +29,20 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter card three numbers(0-4) you want to change: ");
         String[] values = scanner.nextLine().replaceAll("\\D", "").split("");
-        int n = values.length >= 5 ? 5: values.length >= 2 ? 2 : 0;
-        System.out.println("There will be " + n + " cards changed according to the rules.");
-        for (int i = 0; i < n; i++) {
-            int k = Integer.parseInt(values[i]);
+        System.out.println("There will be " + values.length + " cards changed.");
+        for (String value : values) {
+            int k = Integer.parseInt(value);
+            if (k < 0 || k > 5) {
+                System.out.println("Number " + k + " is not within range. Skipping it.");
+                continue;
+            }
             Card cardInHand = this.hand.getCards()[k];
             if (this.deck.canPutThisCardBackToDeck(cardInHand)) {
                 Card c = this.deck.getRandomCard();
                 this.deck.putCardToDeck(cardInHand);
                 this.hand.replaceCard(k, c);
             }
-            System.out.println("-".repeat(10)+values[i]+"-change"+"-".repeat(10));
+            System.out.println("-".repeat(10) + value + "-change" + "-".repeat(10));
             this.hand.printHandInfo();
         }
     }
